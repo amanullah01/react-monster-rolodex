e7908b0c228c86026e3e5de39e2303a98ab73f73
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 14:44:42 +0600

    Updates

---
+++ precache-manifest.b23634127c8382ca7b7aeebc92645054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc5e2e8e56b94bc185ac52cb343f5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1489d296168a3754d55",
+    "url": "/static/css/main.2e4d2626.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1489d296168a3754d55",
+    "url": "/static/js/main.9b105069.chunk.js"
+  },
+  {
+    "revision": "a55fe9c0b312e2774e66",
+    "url": "/static/js/runtime-main.80c5e1e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-monster-rolodex/precache-manifest.0b51d05d971601cd502633169b1a1452.js"
+  "/precache-manifest.b23634127c8382ca7b7aeebc92645054.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-monster-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
