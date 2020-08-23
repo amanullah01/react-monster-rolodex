0f6ea4d91092134767a52c887bc22596fb9d5cc0
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 11:57:01 +0600

    filtering monster

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <input
@@ -29,7 +33,7 @@ class App extends Component {
             )
           }
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
