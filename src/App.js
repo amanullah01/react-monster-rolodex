6712e14732972b138c668f0b97da26c9b6777289
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 08:41:12 +0600

    data show dynamically

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello Amanullah Aman",
+      monsters: [
+        {
+          id: 1,
+          name: "Aman",
+        },
+        {
+          id: 2,
+          name: "Risat",
+        },
+        {
+          id: 3,
+          name: "Asif",
+        },
+        {
+          id: 4,
+          name: "Rohan",
+        },
+        {
+          id: 5,
+          name: "Mamu",
+        },
+      ],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "aamanpro.com" })}>
-            Get the URL
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
