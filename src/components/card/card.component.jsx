2eb1fa4772f75e5d1503543de00ce8b15f202c80
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 11:18:57 +0600

    change image set

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,10 +5,11 @@ export const Card = (props) => {
   return (
     <div className="card-container">
       <img
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x220`}
+        src={`https://robohash.org/${props.monster.id}?set=set${props.monster.id}&size=180x220`}
         alt={props.monster.name}
       />
-      <h2>{props.monster.name}</h2>;<p>{props.monster.email}</p>;
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
