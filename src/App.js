91767269197e00fc40158106f62d9cfa989c3a41
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 11:31:36 +0600

    Search field state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -19,6 +20,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="Search monster"
+          onChange={(e) =>
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            )
+          }
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
