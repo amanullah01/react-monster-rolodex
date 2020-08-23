0337aae984002597afbefa309b6a0922eaf746d3
Amanullah Aman <amancse.aiub@gmail.com>
2020-08-23 11:05:58 +0600

    Card component CSS

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./card.styles.css";
 
 export const Card = (props) => {
-  return <h1>{props.monster.name}</h1>;
+  return (
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x220`}
+        alt={props.monster.name}
+      />
+      <h2>{props.monster.name}</h2>;<p>{props.monster.email}</p>;
+    </div>
+  );
 };
